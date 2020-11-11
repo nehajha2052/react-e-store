610bd940c0d7ce173e38c3ac4af156bba247bf1a
Neha Jha <mailnehajha@gmail.com>
2020-11-11 10:52:49 +0545

    second commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { ProductConsumer } from "../context";
 import { Link } from "react-router-dom";
 import { ButtonContainer } from "./Button";
-import { Button } from "bootstrap";
+
 class Details extends Component {
   state = {};
   render() {
